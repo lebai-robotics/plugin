99a2c741dc15e87c581d486b900beecf47e93a70
rise0chen <rise0chen@163.com>
2023-12-05 18:21:45 +0800

    feat: pyrealsense2

--- apriltag/bin/src/apriltag.py
+++ apriltag/bin/src/apriltag.py
@@ -16,23 +16,31 @@ async def main():
     lebai = await lebai_sdk.connect("127.0.0.1", True)
     camera_index = (await lebai.get_item("plugin_apriltag_camera_index"))['value']
     if not camera_index:
-	    camera_index = "0"
+	    camera_index = "-1"
     camera_index = int(camera_index)
+    camera_width = (await lebai.get_item("plugin_apriltag_camera_width"))['value']
+    if not camera_width:
+	    camera_width = "1280"
+    camera_width = int(camera_width)
+    camera_height = (await lebai.get_item("plugin_apriltag_camera_height"))['value']
+    if not camera_height:
+	    camera_height = "720"
+    camera_height = int(camera_height)
     fx = (await lebai.get_item("plugin_apriltag_fx"))['value']
     if not fx:
-	    fx = "320"
+	    fx = "900"
     fx = float(fx)
     fy = (await lebai.get_item("plugin_apriltag_fy"))['value']
     if not fy:
-	    fy = "320"
+	    fy = "900"
     fy = float(fy)
     cx = (await lebai.get_item("plugin_apriltag_cx"))['value']
     if not cx:
-	    cx = "320"
+	    cx = "640"
     cx = float(cx)
     cy = (await lebai.get_item("plugin_apriltag_cy"))['value']
     if not cy:
-	    cy = "240"
+	    cy = "360"
     cy = float(cy)
     tag_family = (await lebai.get_item("plugin_apriltag_tag_family"))['value']
     if not tag_family:
@@ -43,22 +51,18 @@ async def main():
     tag_size = float(tag_size)
 
     at_detector = apriltag.Detector(families=tag_family)
-    cap = camera.open(camera_index)
-    if not cap:
-        exit(-1)
+    cap = camera.Camera(camera_index, camera_width, camera_height)
+    if not cap.isOpened():
+        exit(1)
 
-    # 开始用摄像头读数据，返回hx为true则表示读成功，frame为读的图像
-    hx, frame = cap.read()
-    hx, frame = cap.read()
-    hx, frame = cap.read()
-    # 如果hx为Flase表示开启摄像头失败，那么就输出"read vido error"并退出程序
-    if hx is False:
-        exit(-1)
+    frame = cap.getImage()
+    time.sleep(1.0)
+    frame = cap.getImage()
+    if frame is None:
+        exit(2)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=(fx, fy, cx, cy), tag_size=tag_size)
 
-    tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=(
-        fx, fy, cx, cy), tag_size=tag_size)
-    
     ret = {}
     for tag in tags:
         pos = tag.pose_t
--- apriltag/bin/src/utils/camera.py
+++ apriltag/bin/src/utils/camera.py
@@ -1,18 +1,63 @@
 # encoding: utf-8
 
+import pyrealsense2 as rs
+import numpy as np
 import cv2
 
-width = 640
-height = 480
+width = 1280
+height = 720
 
 default_index = 0
 
-def open(index = default_index):
-    cap = cv2.VideoCapture(index)  # 打开相机
-    # 检查相机是否成功打开
-    if not cap.isOpened():
-        return None
+class Camera(object):
+    kind = None  # 类型
+    camera = None  # 摄像头实例
+ 
+    def __init__(self, index = default_index, capW = width, capH = height):
+        if index < 0:
+            self.kind = "rs"
+            try:
+                pipeline = rs.pipeline()
+                config = rs.config()
+                config.enable_stream(rs.stream.color, capW, capH, rs.format.bgr8, 15)
+                pipe_profile = pipeline.start(config)
+                self.camera = pipeline
+            except:
+                pass
+        else:
+            self.kind = "cv"
+            try:
+                cap = cv2.VideoCapture(index)
+                if cap.isOpened():
+                    cap.set(cv2.CAP_PROP_FRAME_WIDTH, capW)
+                    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, capH)
+                    self.camera = cap
+            except:
+                pass
+ 
+    def isOpened(self):
+        return self.camera is not None
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    return cap
+    def getImage(self):
+        img_color = None
+        if self.kind == "cv":
+            try:
+                ret, frame = self.camera.read()
+                if ret:
+                    img_color = frame
+            except:
+                pass
+        if self.kind == "rs":
+            try:
+                frames = self.camera.wait_for_frames()
+                color_frame = frames.get_color_frame()
+                img_color = np.asanyarray(color_frame.get_data())
+            except:
+                pass
+        return img_color
+
+    def release(self):
+        if self.kind == "cv":
+            self.camera.release()
+        if self.kind == "rs":
+            self.camera.stop()
