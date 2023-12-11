0048de53c62f59203b24e6cb7c492516c0e34ace
rise0chen <rise0chen@163.com>
2023-12-11 13:45:25 +0800

    feat: apriltag use camera

--- apriltag/bin/src/apriltag.py
+++ apriltag/bin/src/apriltag.py
@@ -2,43 +2,33 @@
 
 ### AprilTag检测位姿
 
+import os
 import cv2
 import time
 import lebai_sdk
-import asyncio
 import utils.rotation as rotation
-import utils.camera as camera
 import dt_apriltags as apriltag
 
-lebai_sdk.init()
+current_dir = os.path.dirname(os.path.abspath(__file__))
+images_dir = os.path.join(current_dir, "../../../camera/images")
 
-async def main():
-    lebai = await lebai_sdk.connect("127.0.0.1", True)
-    camera_index = (await lebai.get_item("plugin_apriltag_camera_index"))['value']
-    if not camera_index:
-        camera_index = "-1"
-    camera_index = int(camera_index)
-    camera_width = (await lebai.get_item("plugin_apriltag_camera_width"))['value']
-    if not camera_width:
-        camera_width = "1280"
-    camera_width = int(camera_width)
-    camera_height = (await lebai.get_item("plugin_apriltag_camera_height"))['value']
-    if not camera_height:
-        camera_height = "720"
-    camera_height = int(camera_height)
-    fx = (await lebai.get_item("plugin_apriltag_fx"))['value']
+lebai_sdk.init()
+lebai = await lebai_sdk.connect("127.0.0.1", True)
+
+def main():
+    fx = (await lebai.get_item("plugin_camera_fx"))['value']
     if not fx:
         fx = "900"
     fx = float(fx)
-    fy = (await lebai.get_item("plugin_apriltag_fy"))['value']
+    fy = (await lebai.get_item("plugin_camera_fy"))['value']
     if not fy:
         fy = "900"
     fy = float(fy)
-    cx = (await lebai.get_item("plugin_apriltag_cx"))['value']
+    cx = (await lebai.get_item("plugin_camera_cx"))['value']
     if not cx:
         cx = "640"
     cx = float(cx)
-    cy = (await lebai.get_item("plugin_apriltag_cy"))['value']
+    cy = (await lebai.get_item("plugin_camera_cy"))['value']
     if not cy:
         cy = "360"
     cy = float(cy)
@@ -51,16 +41,9 @@ async def main():
     tag_size = float(tag_size)
 
     at_detector = apriltag.Detector(families=tag_family)
-    cap = camera.Camera(camera_index, camera_width, camera_height)
-    if not cap.isOpened():
-        exit(1)
-
-    frame = cap.getImage()
-    time.sleep(1.0)
-    frame = cap.getImage()
-    if frame is None:
+    gray = cv2.imread(os.path.join(images_dir, "tmp.jpg"), cv2.IMREAD_GRAYSCALE)
+    if not gray:
         exit(2)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=(fx, fy, cx, cy), tag_size=tag_size)
 
     ret = {}
@@ -71,7 +54,5 @@ async def main():
         ret[tag.tag_id] = offset
 
     print(ret)
-    # 释放摄像头
-    cap.release()
 
-asyncio.run(main())
+main()
