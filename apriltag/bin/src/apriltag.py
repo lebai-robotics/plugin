1213c96d6fe72523fbaac921ff3570bd1cf90e49
rise0chen <rise0chen@163.com>
2023-12-11 18:03:52 +0800

    fix: await

--- apriltag/bin/src/apriltag.py
+++ apriltag/bin/src/apriltag.py
@@ -13,36 +13,36 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(current_dir, "../../../camera/images")
 
 lebai_sdk.init()
-lebai = await lebai_sdk.connect("127.0.0.1", True)
+lebai = lebai_sdk.connect("127.0.0.1", True)
 
 def main():
-    fx = (await lebai.get_item("plugin_camera_fx"))['value']
+    fx = (lebai.get_item("plugin_camera_fx"))['value']
     if not fx:
         fx = "900"
     fx = float(fx)
-    fy = (await lebai.get_item("plugin_camera_fy"))['value']
+    fy = (lebai.get_item("plugin_camera_fy"))['value']
     if not fy:
         fy = "900"
     fy = float(fy)
-    cx = (await lebai.get_item("plugin_camera_cx"))['value']
+    cx = (lebai.get_item("plugin_camera_cx"))['value']
     if not cx:
         cx = "640"
     cx = float(cx)
-    cy = (await lebai.get_item("plugin_camera_cy"))['value']
+    cy = (lebai.get_item("plugin_camera_cy"))['value']
     if not cy:
         cy = "360"
     cy = float(cy)
-    tag_family = (await lebai.get_item("plugin_apriltag_tag_family"))['value']
+    tag_family = (lebai.get_item("plugin_apriltag_tag_family"))['value']
     if not tag_family:
         tag_family = "tag36h11"
-    tag_size = (await lebai.get_item("plugin_apriltag_tag_size"))['value']
+    tag_size = (lebai.get_item("plugin_apriltag_tag_size"))['value']
     if not tag_size:
         tag_size = "0.04"
     tag_size = float(tag_size)
 
     at_detector = apriltag.Detector(families=tag_family)
     gray = cv2.imread(os.path.join(images_dir, "tmp.jpg"), cv2.IMREAD_GRAYSCALE)
-    if not gray:
+    if gray.size == 0:
         exit(2)
     tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=(fx, fy, cx, cy), tag_size=tag_size)
 
