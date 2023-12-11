17892f1fe3e5f792c9f201eeedf26f168d93abe5
rise0chen <rise0chen@163.com>
2023-12-11 17:37:45 +0800

    fix: main

--- apriltag/bin/src/apriltag.py
+++ apriltag/bin/src/apriltag.py
@@ -55,4 +55,5 @@ def main():
 
     print(ret)
 
-main()
+if __name__ == '__main__':
+    main()
--- camera/bin/src/camera.py
+++ camera/bin/src/camera.py
@@ -69,4 +69,5 @@ def main():
     # 释放摄像头
     cap.release()
 
-main()
+if __name__ == '__main__':
+    main()
