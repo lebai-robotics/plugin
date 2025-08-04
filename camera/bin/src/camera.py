147e2ea5699b9b6d72928155b29215f824ac965f
rise0chen <rise0chen@163.com>
2025-08-04 11:04:27 +0800

    fix: flip

--- camera/bin/src/camera.py
+++ camera/bin/src/camera.py
@@ -97,7 +97,8 @@ def main():
             flip = get_flip()
             if flip in [0, 1, -1]:
                 frame = cv2.flip(frame, flip)
-                frame = cv2.flip(depth, flip)
+                if depth:
+                    depth = cv2.flip(depth, flip)
             cv2.imwrite(os.path.join(images_dir, "img.tmp.jpg"), frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
             shutil.move(os.path.join(images_dir, "img.tmp.jpg"), os.path.join(images_dir, "img.jpg"))
             if depth:
