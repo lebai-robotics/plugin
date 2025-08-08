9a0bcd1c8408da4e0a6adacfbfb266cd39090e60
rise0chen <rise0chen@163.com>
2025-08-08 16:11:47 +0800

    camera clearCache

--- camera/bin/src/utils/camera.py
+++ camera/bin/src/utils/camera.py
@@ -2,6 +2,7 @@
 
 import pyrealsense2 as rs
 import numpy as np
+import concurrent.futures
 import cv2
 
 width = 1280
@@ -14,7 +15,7 @@ class Camera(object):
     camera = None  # 摄像头实例
  
     def __init__(self, index = default_index, capW = width, capH = height, fps = fps):
-        if index < 0:
+        if index == -1:
             self.kind = "rs"
             try:
                 pipeline = rs.pipeline()
@@ -41,12 +42,23 @@ class Camera(object):
     def isOpened(self):
         return self.camera is not None
 
+    def clearCache(self):
+        if self.kind == "cv":
+            with concurrent.futures.ThreadPoolExecutor(max_workers=1) as ex:
+                while True:
+                    try:
+                        future = ex.submit(self.camera.grab)
+                        if not future.result(timeout=0.5/fps):
+                            break
+                    except concurrent.futures.TimeoutError:
+                        break
+
     def getImage(self):
         img_color = None
         img_depth = None
         if self.kind == "cv":
             try:
-                self.camera.read()
+                self.clearCache()
                 ret, frame = self.camera.read()
                 if ret:
                     img_color = frame
